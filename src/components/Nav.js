dd58a50631623b1de4d03264fee882f44e9afba0
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-13 10:18:24 +0300

    Nav bar component complete.

---
+++ src/components/Nav.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import "./Nav.css";
+
+const Nav = () => {
+
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <nav className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+        alt="netflix logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt="message logo"
+      />
+    </nav>
+  );
+};
+
+export default Nav;
