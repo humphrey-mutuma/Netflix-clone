2349de72ce78165f67ea14e6f99e9f612c939507
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-13 11:32:10 +0300

    Display trailers on click complete.

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
+import YouTube from "react-youtube";
 import axios from "../axios";
 import "./Row.css";
+import  movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
-
+  const[trailerUrl, setTrailerUrl] = useState("")
+ 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -17,6 +20,39 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
   }, [fetchUrl]);
 
   // console.log(movies);
+const opts ={
+  height:'390',
+  width:'100%',
+  playerVars:{
+    // https://developers.google.com/youtube/player_parameters
+    autoPlay:1
+  }
+}
+
+const handleClick = (movie) => {
+  if(trailerUrl){
+    setTrailerUrl("");
+  }else{
+    movieTrailer(movie?.name || "")
+    .then((url) => {
+      //
+      const urlParams = new URLSearchParams(new URL(url).search);
+      setTrailerUrl(urlParams.get("v"));
+    })
+    .catch(error => console.log(error));
+  }
+}
+  // const handleClick = (movie) => {
+  //   if (trailerUrl != "") setTrailerUrl("");
+  //   else {
+  //     movieTrailer(movie)
+  //       .then((url) => {
+  //         const urlParamV = new URLSearchParams(new URL(url).search);
+  //         setTrailerUrl(urlParamV.get("v"));
+  //       })
+  //       .catch((err) => console.log(err));
+  //   }
+  // };
 
   return (
     <div className="row">
@@ -24,6 +60,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
       <div className="row_posters">
         {movies.map((movie) => (
           <img
+            onClick={() => handleClick(movie)}
             key={movie.id}
             className={` row_poster ${isLargeRow && "row_posterLarge "}`}
             src={`${base_url}${
@@ -33,6 +70,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
